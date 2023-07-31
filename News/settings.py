68794f68494f4bf15526845faf2dd2d3ed785ccf
RUNRanjit <88085245+RUNRanjit@users.noreply.github.com>
2023-08-01 00:17:53 +0530

    Second Commit

--- News/settings.py
+++ News/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_otjd(d37(0#3qpupy&09@45mg==-7gnc4oyczu2)n#%q=k)qc'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -124,4 +125,4 @@ STATICFILES_DIRS =os.path.join(BASE_DIR, 'static'),
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-API_KEY = '30783dce148d499691b531373ef9523a'
+API_KEY = config('API_KEY')
