bdad3ecae2127fefd67277dd08b1dce4103b261c
RUNRanjit <88085245+RUNRanjit@users.noreply.github.com>
2023-08-01 00:06:57 +0530

    Initial Commit

---
+++ newsapp/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+import requests
+from News.settings import API_KEY
+
+
+def home_view(request):
+    country = request.GET.get('country')
+    category = request.GET.get('category')
+
+    if country:
+        url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
+
+    else:
+        url = f'https://newsapi.org/v2/top-headlines?category={category}&apiKey={API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
+
+
+
+    context = {
+        'articles' : articles
+    }
+
+    return render(request, 'home.html', context)
